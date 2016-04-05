c23b05bc386ef31a0fb550f43aa1e985c0b30eee
Alan Freire <alan_freire@msn.com>
2016-04-05 13:55:01 -0300

    Troca de tabs por espaços
    Formatação do código, troca de tabs por espaços de acordo com a PSR-2

--- PDO/Connect.class.php
+++ PDO/Connect.class.php
@@ -1,25 +1,25 @@
 <?php
 class Connect
 {
-	public static $instance;
-	
-	private function __construct()
-	{
-		try {
-			self::$instance = new PDO('mysql:host=localhost;dbname=db_inventory;', 'root', 'livre', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
-		} catch (PDOException $e) {
-			die('Erro no banco de dados: '.$e->getMessage());
-		}
-	}
-	
-	public static function get_instance()
-	{
-		if (!isset(self::$instance)) {
-			new Connect();
-		}
-		
-		return self::$instance;
-	}
+    public static $instance;
+    
+    private function __construct()
+    {
+        try {
+            self::$instance = new PDO('mysql:host=localhost;dbname=db_inventory;', 'root', 'livre', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        } catch (PDOException $e) {
+            die('Erro no banco de dados: '.$e->getMessage());
+        }
+    }
+    
+    public static function get_instance()
+    {
+        if (!isset(self::$instance)) {
+            new Connect();
+        }
+        
+        return self::$instance;
+    }
 }
